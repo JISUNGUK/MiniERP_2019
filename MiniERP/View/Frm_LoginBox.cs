62cdfbcfe4bbaae5b021ed3ba5f859f3c23b4247
JSU <totalrook@gmail.com>
2019-02-14 11:57:51 +0900

    LoginBox Form 의 로그인부 추가, 추후 DB연결 재만들어야함/ 중복 Login폼 삭제

--- MiniERP/View/Frm_LoginBox.cs
+++ MiniERP/View/Frm_LoginBox.cs
@@ -12,13 +12,22 @@ namespace MiniERP.View
 {
     public partial class Frm_LoginBox : Form
     {
-        public Frm_LoginBox()
+        int test2 = 0;
+        private Form1 form1;
+
+        public Frm_LoginBox(Form1 form1)
         {
             InitializeComponent();
+            this.form1 = form1;
         }
 
         private void btn_Login_Click(object sender, EventArgs e)
         {
+            if (true)
+            {
+                MessageBox.Show("로그인성공");
+                form1.logIn = DialogResult.OK;
+            }
             this.Close();
         }
 
