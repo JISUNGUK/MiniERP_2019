de3c396f18374f2564f7b31034f24a3cdaae4df5
ccarayhua <sabisimono@gmail.com>
2019-02-15 16:17:46 +0900

    파일이미지추가

---
+++ MiniERP/Model/DAO/win32.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace MiniERP.Model.DAO
+{
+    class Win32
+    {
+        public const uint SHGFI_ICON = 0x100;
+        public const uint SHGFI_LARGEICON = 0x0;
+        public const uint SHGFI_SMALLICON = 0x1;
+
+        [DllImport("shell32.dll")]
+        public static extern IntPtr SHGetFileInfo(string pszPath,
+               uint dwFileAttributes,
+               ref View.Form1.SHFILEINFO psfi,
+               uint cbSizeFileInfo,
+               uint uFlags);
+    }
+}
