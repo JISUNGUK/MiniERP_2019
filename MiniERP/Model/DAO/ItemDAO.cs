9224e5efc38de2c2461bd2817eb30d0bbd3afa88
LSK078 <lskt078@gmail.com>
2019-02-28 18:20:28 +0900

    삭제

--- MiniERP/Model/DAO/ItemDAO.cs
+++ MiniERP/Model/DAO/ItemDAO.cs
@@ -39,7 +39,7 @@ namespace MiniERP.Model.DAO
                     item.Item_class = sr["Item_class"].ToString();
                     item.Item_fee = Int32.Parse(sr["Item_fee"].ToString());
                     item.Stock_count = Int32.Parse(sr["Stock_count"].ToString());
-                    // item.Item_image
+                    //item.Item_image = 
                     item.Item_group = sr["Item_group"].ToString();
                     item.Item_comment = sr["Item_comment"].ToString();
                     items.Add(item);
@@ -81,6 +81,47 @@ namespace MiniERP.Model.DAO
             try
             {
                 return con.ExecuteNonQuery(procedureName, sqlParameters);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public int DeleteItem(string itemCode)
+        {
+            string storedProcedureName = "DeleteItem";
+            SqlParameter[] sqlParameters =
+            {
+                new SqlParameter("item_code", itemCode)
+            };
+            DBConnection con = new DBConnection();
+            try
+            {
+                return con.ExecuteNonQuery(storedProcedureName, sqlParameters);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// 등록하려는 Item의 code가 이미 Item테이블에 존재하는지 확인하는 메서드입니다.
+        /// </summary>
+        /// <param name="itemCode">새롭게 등록할 Item의 code입니다.</param>
+        /// <returns>결과 행이 존재하면 true, 그렇지 않으면 false를 반환합니다.</returns>
+        public bool CheckItemData(string itemCode)
+        {
+            string storedProcedureName = "Check_ItemData";
+            SqlParameter[] sqlParameters =
+            {
+                new SqlParameter("item_code", itemCode)
+            };
+            DBConnection con = new DBConnection();
+            try
+            {
+                return con.ExecuteSelect(storedProcedureName, sqlParameters).HasRows;
             }
             catch (Exception)
             {
---
+++ MiniERP/View/StockManagement/Frm_ItemInfo.cs
@@ -0,0 +1,35 @@
+﻿using MiniERP.Model.DAO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MiniERP.VO;
+
+namespace MiniERP.View.StockManagement
+{
+    public partial class Frm_ItemInfo : Form
+    {
+        private List<Item> items;
+        private string itemCode; // 부모폼으로부터 얻어오는 item의 code입니다.
+
+        public Frm_ItemInfo()
+        {
+            InitializeComponent();
+        }
+
+        public Frm_ItemInfo(string itemCode)
+        {
+            this.itemCode = itemCode;
+        }
+
+        private void Frm_ItemInfo_Load(object sender, EventArgs e)
+        {
+            //new ItemDAO().GetItems(itemCode)
+        }
+    }
+}
