68e4b4e5c92ff1468adbd1f6c3e463e1b5efd1ca
tokoyy <totalrook@gmail.com>
2019-03-02 07:01:05 +0900

    머신클라이언트 수정중/ 옵션만들던것

--- MiniErp_Client_jsu/MiniErp_Client_jsu/Form1.cs
+++ MiniErp_Client_jsu/MiniErp_Client_jsu/Form1.cs
@@ -17,7 +17,6 @@ namespace MiniErp_Client_jsu
         Machine machine = new Machine();
         Chatting chatting;
 
-
         //  erro , command 리스트
         List<Erro> erros = new List<Erro>();
         List<Command> commands = new List<Command>();
@@ -26,10 +25,28 @@ namespace MiniErp_Client_jsu
         public Form1()
         {
             InitializeComponent();
-            chatting = new Chatting(machine.Ip, machine.Name,codes);
+            this.Text = machine.Name;
+            this.TopMost = true;
+            chatting = new Chatting(machine.Ip, machine.Name, codes);
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("---------[투입 현황]");
+            sb.AppendLine("---------[2019-03-02 6:44:50]");
+            sb.AppendLine("*[1234]	1");
+            sb.AppendLine("*[1235]	1");
+            sb.AppendLine("----------------------------");
+
+            //  바코드 문자열잘라줄때쓰자
+            string temp = sb.ToString().Remove(0, sb.ToString().IndexOf('*')).Replace("-", "").Trim();
+            string[] temp2 = temp.Split('*');
+            MessageBox.Show(temp);
+            foreach (var item in temp2)
+            {
+                MessageBox.Show(item);
+            }
         }
 
-        
+
 
         private void txt_Barcode_KeyDown(object sender, KeyEventArgs e)
         {
@@ -68,19 +85,48 @@ namespace MiniErp_Client_jsu
         private void button1_Click(object sender, EventArgs e)
         {
             StringBuilder sb = new StringBuilder();
+            sb.AppendLine("---------[투입 현황]");
+            sb.AppendLine(NowTime());
             foreach (var item in codes)
             {
                 txt_Log.Clear();
-                sb.AppendLine(item.Barcode_Code + "\t" + item.Barcode_Count);
+                sb.AppendLine("*["+item.Barcode_Code+"]" + "\t" + item.Barcode_Count);
             }
+            sb.AppendLine("----------------------------");
             txt_Log.Text = sb.ToString();
 
             this.txt_Barcode.Focus();
         }
 
+        private string NowTime()
+        {
+            DateTime dt = DateTime.Now;
+            return
+                "---------[" + dt.ToShortDateString() + " " + dt.Hour + ":" + dt.Minute + ":" + dt.Second + "]";
+        }
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             chatting.CloseSeverTest();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            switch (e.ClickedItem.ToString())
+            {
+                case "화면고정":
+                    if (this.TopMost != true)
+                        this.TopMost = true;
+                    else
+                        this.TopMost = false;
+                    break;
+
+                case "종료":  this.Close();   break;
+                case "재시작":  Application.Restart();   break;
+                case "자재투입현황":  button1_Click(null, null);   break;
+
+                default:
+                    break;
+            }
         }
     }
 }
