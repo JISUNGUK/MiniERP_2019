cab1a8ce46b74d71f99b5193a09811bbaa41b814
ccarayhua <sabisimono@gmail.com>
2019-03-02 12:09:37 +0900

    생산계획 세부검색 없앰

--- MiniERP/View/LogisticsManagement/Frm_productionList.Designer.cs
+++ MiniERP/View/LogisticsManagement/Frm_productionList.Designer.cs
@@ -30,24 +30,11 @@
         {
             this.produceGrid = new System.Windows.Forms.DataGridView();
             this.exportExcel = new System.Windows.Forms.Button();
-            this.btnFind = new System.Windows.Forms.Button();
-            this.itemcount = new System.Windows.Forms.NumericUpDown();
-            this.chkCase = new System.Windows.Forms.CheckBox();
-            this.chkFrag = new System.Windows.Forms.CheckBox();
-            this.itemSearch = new System.Windows.Forms.Button();
             this.orderSearch = new System.Windows.Forms.Button();
-            this.label11 = new System.Windows.Forms.Label();
-            this.itemcode = new System.Windows.Forms.TextBox();
-            this.label8 = new System.Windows.Forms.Label();
             this.ordercode = new System.Windows.Forms.TextBox();
             this.searchPlan = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.chkMob = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.produceGrid)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.itemcount)).BeginInit();
-            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // produceGrid
@@ -56,11 +43,11 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.produceGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.produceGrid.Location = new System.Drawing.Point(24, 301);
+            this.produceGrid.Location = new System.Drawing.Point(24, 88);
             this.produceGrid.Margin = new System.Windows.Forms.Padding(5);
             this.produceGrid.Name = "produceGrid";
             this.produceGrid.RowTemplate.Height = 23;
-            this.produceGrid.Size = new System.Drawing.Size(1773, 667);
+            this.produceGrid.Size = new System.Drawing.Size(1773, 880);
             this.produceGrid.TabIndex = 78;
             // 
             // exportExcel
@@ -76,61 +63,6 @@
             this.exportExcel.UseVisualStyleBackColor = true;
             this.exportExcel.Click += new System.EventHandler(this.exportExcel_Click);
             // 
-            // btnFind
-            // 
-            this.btnFind.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnFind.Location = new System.Drawing.Point(1129, 32);
-            this.btnFind.Margin = new System.Windows.Forms.Padding(5);
-            this.btnFind.Name = "btnFind";
-            this.btnFind.Size = new System.Drawing.Size(118, 40);
-            this.btnFind.TabIndex = 151;
-            this.btnFind.Text = "찾기";
-            this.btnFind.UseVisualStyleBackColor = true;
-            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
-            // 
-            // itemcount
-            // 
-            this.itemcount.Location = new System.Drawing.Point(714, 35);
-            this.itemcount.Margin = new System.Windows.Forms.Padding(5);
-            this.itemcount.Name = "itemcount";
-            this.itemcount.Size = new System.Drawing.Size(396, 32);
-            this.itemcount.TabIndex = 150;
-            // 
-            // chkCase
-            // 
-            this.chkCase.AutoSize = true;
-            this.chkCase.Location = new System.Drawing.Point(423, 106);
-            this.chkCase.Margin = new System.Windows.Forms.Padding(5);
-            this.chkCase.Name = "chkCase";
-            this.chkCase.Size = new System.Drawing.Size(99, 25);
-            this.chkCase.TabIndex = 149;
-            this.chkCase.Text = "케이스";
-            this.chkCase.UseVisualStyleBackColor = true;
-            // 
-            // chkFrag
-            // 
-            this.chkFrag.AutoSize = true;
-            this.chkFrag.Location = new System.Drawing.Point(316, 106);
-            this.chkFrag.Margin = new System.Windows.Forms.Padding(5);
-            this.chkFrag.Name = "chkFrag";
-            this.chkFrag.Size = new System.Drawing.Size(78, 25);
-            this.chkFrag.TabIndex = 148;
-            this.chkFrag.Text = "부품";
-            this.chkFrag.UseVisualStyleBackColor = true;
-            // 
-            // itemSearch
-            // 
-            this.itemSearch.BackgroundImage = global::MiniERP.Properties.Resources.SerchIcon;
-            this.itemSearch.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.itemSearch.Font = new System.Drawing.Font("굴림", 10F);
-            this.itemSearch.Location = new System.Drawing.Point(538, 28);
-            this.itemSearch.Margin = new System.Windows.Forms.Padding(5);
-            this.itemSearch.Name = "itemSearch";
-            this.itemSearch.Size = new System.Drawing.Size(39, 44);
-            this.itemSearch.TabIndex = 146;
-            this.itemSearch.UseVisualStyleBackColor = true;
-            this.itemSearch.Click += new System.EventHandler(this.itemSearch_Click);
-            // 
             // orderSearch
             // 
             this.orderSearch.BackgroundImage = global::MiniERP.Properties.Resources.SerchIcon;
@@ -143,36 +75,6 @@
             this.orderSearch.TabIndex = 145;
             this.orderSearch.UseVisualStyleBackColor = true;
             this.orderSearch.Click += new System.EventHandler(this.orderSearch_Click);
-            // 
-            // label11
-            // 
-            this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(18, 110);
-            this.label11.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(52, 21);
-            this.label11.TabIndex = 143;
-            this.label11.Text = "규격";
-            // 
-            // itemcode
-            // 
-            this.itemcode.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.itemcode.Location = new System.Drawing.Point(178, 30);
-            this.itemcode.Margin = new System.Windows.Forms.Padding(5);
-            this.itemcode.Name = "itemcode";
-            this.itemcode.Size = new System.Drawing.Size(320, 32);
-            this.itemcode.TabIndex = 142;
-            // 
-            // label8
-            // 
-            this.label8.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(652, 35);
-            this.label8.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(52, 21);
-            this.label8.TabIndex = 141;
-            this.label8.Text = "수량";
             // 
             // ordercode
             // 
@@ -194,17 +96,6 @@
             this.searchPlan.UseVisualStyleBackColor = true;
             this.searchPlan.Click += new System.EventHandler(this.searchPlan_Click);
             // 
-            // label1
-            // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(18, 35);
-            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(94, 21);
-            this.label1.TabIndex = 138;
-            this.label1.Text = "품목코드";
-            // 
             // label5
             // 
             this.label5.AutoSize = true;
@@ -215,40 +106,11 @@
             this.label5.TabIndex = 137;
             this.label5.Text = "주문코드";
             // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.itemcode);
-            this.panel1.Controls.Add(this.chkCase);
-            this.panel1.Controls.Add(this.btnFind);
-            this.panel1.Controls.Add(this.chkFrag);
-            this.panel1.Controls.Add(this.itemSearch);
-            this.panel1.Controls.Add(this.chkMob);
-            this.panel1.Controls.Add(this.itemcount);
-            this.panel1.Controls.Add(this.label8);
-            this.panel1.Controls.Add(this.label11);
-            this.panel1.Location = new System.Drawing.Point(24, 113);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1252, 167);
-            this.panel1.TabIndex = 152;
-            // 
-            // chkMob
-            // 
-            this.chkMob.AutoSize = true;
-            this.chkMob.Location = new System.Drawing.Point(177, 106);
-            this.chkMob.Margin = new System.Windows.Forms.Padding(5);
-            this.chkMob.Name = "chkMob";
-            this.chkMob.Size = new System.Drawing.Size(99, 25);
-            this.chkMob.TabIndex = 147;
-            this.chkMob.Text = "휴대폰";
-            this.chkMob.UseVisualStyleBackColor = true;
-            // 
             // Frm_productionList
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1811, 1026);
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.exportExcel);
             this.Controls.Add(this.orderSearch);
             this.Controls.Add(this.ordercode);
@@ -261,9 +123,6 @@
             this.Load += new System.EventHandler(this.Frm_productionList_Load);
             this.Resize += new System.EventHandler(this.Frm_productionList_Resize);
             ((System.ComponentModel.ISupportInitialize)(this.produceGrid)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.itemcount)).EndInit();
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -272,20 +131,9 @@
         #endregion
         private System.Windows.Forms.DataGridView produceGrid;
         private System.Windows.Forms.Button exportExcel;
-        private System.Windows.Forms.Button btnFind;
-        private System.Windows.Forms.NumericUpDown itemcount;
-        private System.Windows.Forms.CheckBox chkCase;
-        private System.Windows.Forms.CheckBox chkFrag;
-        private System.Windows.Forms.Button itemSearch;
         private System.Windows.Forms.Button orderSearch;
-        private System.Windows.Forms.Label label11;
-        private System.Windows.Forms.TextBox itemcode;
-        private System.Windows.Forms.Label label8;
         private System.Windows.Forms.TextBox ordercode;
         private System.Windows.Forms.Button searchPlan;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.CheckBox chkMob;
     }
 }
